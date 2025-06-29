5f81df737fb2ef42989f21be609c12337cf23f82
Bakar Mokhtar Ali <Abukarmacalin08@gmail.com>
2025-06-29 12:18:49 +0300

    added batch to a academic model

--- src/controllers/academic.controller.ts
+++ src/controllers/academic.controller.ts
@@ -7,7 +7,8 @@ import { AuthRequest } from "../middleware/protect";
 // CREATE Academic Year
 export const createAcademic = catchAsync(
   async (req: AuthRequest, res: Response, next: NextFunction) => {
-    const { academic_year, semester_id, start_date, end_date } = req.body;
+    const { academic_year, semester_id, batch_id, start_date, end_date } =
+      req.body;
 
     if (!academic_year || !semester_id || !start_date || !end_date) {
       return next(new AppError("All fields are required", 400));
@@ -18,18 +19,19 @@ export const createAcademic = catchAsync(
       let exists = true;
 
       while (exists) {
-        const id = `TTB${academinId}`;
+        const id = `ACC${academinId}`;
         exists = !!(await Academic.exists({ academic_id: id }));
         if (exists) academinId++;
       }
 
-      return `TTB${academinId}`;
+      return `ACC${academinId}`;
     };
     const academic_id = await generateacademicId();
 
     const academic = await Academic.create({
       academic_id,
       academic_year,
+      batch_id,
       semester_id,
       start_date,
       end_date,
@@ -45,7 +47,9 @@ export const createAcademic = catchAsync(
 // GET all academic sessions
 export const getAllAcademic = catchAsync(
   async (req: Request, res: Response) => {
-    const academicList = await Academic.find().populate("semester_id");
+    const academicList = await Academic.find()
+      .populate("semester_id")
+      .populate("batch_id");
 
     res.status(200).json({
       status: "success",
--- src/models/academic.model.ts
+++ src/models/academic.model.ts
@@ -3,7 +3,8 @@ import mongoose, { Schema, Document } from "mongoose";
 export interface IAcademic extends Document {
   academic_id: string;
   academic_year: string;
-  semester_id: string; // reference to Semester model
+  semester_id?: string; // reference to Semester model
+  batch_id: string;
   start_date: Date;
   end_date: Date;
 }
@@ -12,10 +13,10 @@ const academicSchema: Schema = new Schema(
   {
     academic_id: { type: String, required: true, unique: true },
     academic_year: { type: String, required: true },
+    batch_id: { type: String, ref: "Batch" },
     semester_id: {
       type: Schema.Types.ObjectId,
       ref: "Semester", // 👈 points to your Semester model
-      required: true,
     },
     start_date: { type: Date, required: true },
     end_date: { type: Date, required: true },
